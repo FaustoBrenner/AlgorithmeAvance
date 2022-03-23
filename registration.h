9c4fe1ebdd9f982d0e3f74ee3c6cf24fe4c9620c
FaustoBrenner <32939930+FaustoBrenner@users.noreply.github.com>
2022-03-23 17:28:32 +0100

    Add files via upload

--- registration.h
+++ registration.h
@@ -1,13 +1,15 @@
 #include "nr3.h"
 #include "amoeba.h"
 
+// Partie II
 struct Point
 {
     int x, y;
     Point(VecDoub point);
-    VecDoub toVecDoub();
+    VecDoub toVecDoub();    
 };
 
+// Partie II
 struct Parameters
 {
     double tx, ty, theta;
@@ -16,54 +18,80 @@ struct Parameters
     VecDoub toVecDoub();
 };
 
+// Partie I
 class Image
 {
 public:
-    NRmatrix im;
-    NRmatrix mask;
+    NRmatrix<double> im;
+    NRmatrix<double> mask;
 
     Image(string filename);
     Image(int rows, int cols);
 
-    NRmatrix readFromFile(string filename);
+    void readFromFile(string filename, NRmatrix<double> &img);
     void writeToFile(string filename);
 
     int nrows();
     int ncols();
 
-    Image deform(Parameters param, InterpolationFunction interpolation);
+    void deform(NRmatrix<double> &img, Parameters param, InterpolationFunction *interpolation, Transform *trans);
 
 private:
-    Point applyTransformation(Point p, Parameters param);
+    Point applyTransformation(Point p, Parameters param, Transform *trans);
 };
 
-
-
-class Function
+class Transform
 {
     public : 
-	virtual double operator () (double) const = 0;
+	virtual double operator () (Point p, Parameters param) const = 0;
 };
 
-class SimilarityCriterium : Function
+class InterpolationFunction
+{
+    public : 
+	virtual double operator () (Image &image, Point p, int &OK) const = 0;
+};
+
+class SimilarityCriterium
+{
+    public : 
+	virtual double operator () (Image &im1, Image &im2) const = 0;
+};
+
+class CostFunction
+{
+    public : 
+	virtual double operator () (Parameters param) const = 0;
+};
+
+class MyTransform : Transform
+{
+    public : 
+	double operator () (Point p, Parameters param);
+};
+
+class MySimilarityCriterium : SimilarityCriterium
 {
 public :
-	double operator () (Image im1, Image im2);
+	double operator () (Image &im1, Image &im2);
 };
 
-class InterpolationFunction : Function
+class MyInterpolationFunction : InterpolationFunction
 {
 public :
-	double operator () (Image image, Point p, int *OK);
+	double operator () (Image image, Point p, int &OK);
 };
 
-class CostFunction : Function
+class MyCostFunction : CostFunction
 {
 public :
-	double operator () (Image I, Image Iref, Parameters param, SimilarityCriterium similarity, InterpolationFunction interpolation);
+	double operator () (Parameters param);
 };
 
-Parameters registration(Image I, Image Iref, SimilarityCriterium similarity, InterpolationFunction interpolation);
+// Partie IV
+Parameters optimize(CostFunction *costFunc, Image &I, Image &Iref, Parameters param);
 
-Parameters optimize(CostFunction costFunc, Image I, Image Iref, Parameters param);
+// Partie V
+Parameters registration(Image &I, Image &Iref, SimilarityCriterium *similarity, InterpolationFunction *interpolation);
+
 
