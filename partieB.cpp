0ffbb34c2893060a70b28d96fb131af13f5970dd
FAUSTO BRENNER <faustoemrich@outlook.com>
2022-04-05 14:00:38 +0200

    Partie B - Leo

---
+++ partieB.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "registration.h"
+
+
+
+
+Image::Image()
+{
+  im.assign(1,1,0);
+  mask.assign(1,1,0);
+  
+}
+
+
+Image::Image(string filename)
+{
+    readFromFile(im,filename);
+    readFromFile(mask,filename);
+    mask.assign(mask.nrows(),mask.ncols(),0);
+}
+Image::Image(int rows, int cols)
+{
+    im.assign(rows,cols,0);
+    mask.assign(rows,cols,0);
+}
+
+
+int Image::nrows() const
+{
+  return im.nrows();
+}
+int Image::ncols() const
+{
+  return im.ncols();
+}
+
+
+void Image::deform(Image &outImg, const Parameters &param, InterpolationFunction *interpolation, Transform *transform) const
+{
+  outImg.im.resize(nrows(),ncols());
+  for (int i=0;i<nrows();i++)
+    {for (int j=0;j<ncols();j++)
+      {VecDoub vec(i,j);
+      Point p(vec);
+      Point p_trans = (*transform)(p,param);
+      int ok=0;
+      outImg.im[i][j] = (*interpolation)(*this,p_trans,ok);
+      }
+      }
+    }
