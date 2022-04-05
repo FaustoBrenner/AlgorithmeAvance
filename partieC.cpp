ac0a14388befaeba693bba4015a148a1eaa38636
Artur <arturwaquil@gmail.com>
2022-04-05 14:58:18 +0200

    Update partieC.cpp

--- partieC.cpp
+++ partieC.cpp
@@ -1,26 +1,23 @@
 #include "registration.h"
-#include <math.h>
+#include <cmath>
 #include "nr3.h"
-
-#define PI 3.14159265
 
 Point MyTransform :: operator () (Point p, const Parameters &param)
 {
     int new_x = (int) param.tx
-                        + p.x*cos(param.theta*PI/180) - 
-                        - p.y*sin(param.theta*PI/180);
+                        + p.x*cos(param.theta*M_PI/180) - 
+                        - p.y*sin(param.theta*M_PI/180);
 
     int new_y = (int) param.ty
-                        + p.y*cos(param.theta*PI/180) - 
-                        + p.x*sin(param.theta*PI/180);
+                        + p.y*cos(param.theta*M_PI/180) - 
+                        + p.x*sin(param.theta*M_PI/180);
     
     Point new_p = Point(new_x, new_y);
 
     return new_p;
 }
 
-void run(const Image &I, const Image &Iref, const Parameters &inParam, Parameters &outParam, 
-CostFunction costFunc, SimilarityCriterium *similarity, InterpolationFunction *interpolation)
+void run(const Parameters &inParam, Parameters &outParam, CostFunction costFunc)
 {
     Amoeba opt(0.01);
     VecDoub finalParam(3);
